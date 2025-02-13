b6f7a530b293f7079d16a0d10721b11bccb8aeba
Marylin Rosa <marylin.rosa.853@gmail.com>
2025-02-13 15:13:50 -0500

    Revert "Added new line under Hello World"
    This reverts commit 0c4ef51c5f845084f6882e480a882a801b9f63b6.

--- HelloWorldForGitHub.java
+++ HelloWorldForGitHub.java
@@ -2,7 +2,6 @@ public class HelloWorldForGitHub
 {
    public static void main(String [] args)
    {
-      System.out.println("Hello World!");
-      System.out.println("Program modified, but able to revert to original version using Github Desktop.");
+      System.out.print("Hello World!");
    }
 }
