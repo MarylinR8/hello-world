0673aae484ea61f55b43bceccc061af09def95df
Marylin Rosa <marylin.rosa.853@gmail.com>
2026-02-15 22:43:11 -0500

    Revert "Program was modified"
    This reverts commit 33b0e9f96fe0e402fb1a42522a05cb1cffd827a0.

--- helloWorldDemo.java
+++ helloWorldDemo.java
@@ -3,6 +3,5 @@ public class helloWorldDemo
    public static void main(String [] args)
    {
       System.out.println("Hello world!");
-      System.out.println("Program modified, but able to revert to original version using Github Desktop"); 
    }
 }
